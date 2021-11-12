f2ce8da44188ac573d44674a21ddf4571b2f49a7
Defal09 <09defal@gmail.com>
2021-11-11 23:04:11 -0300

    Change to updated visual guides

--- js/index.js
+++ js/index.js
@@ -1,54 +1,74 @@
-const menuItems = [...document.getElementsByClassName("menu-item")];
+// const menuItems = [...document.getElementsByClassName("menu-item")];
 
-menuItems.forEach((item) =>
-    item.addEventListener("click", () => setActiveSection(item.id))
-);
+// menuItems.forEach((item) =>
+//     item.addEventListener("click", () => setActiveSection(item.id))
+// );
 
-function removeAllActiveSections() {
-    menuItems.forEach((element) => {
-        element.classList.remove("active-section");
-    });
-}
+// function removeAllActiveSections() {
+//     menuItems.forEach((element) => {
+//         element.classList.remove("active-section");
+//     });
+// }
 
-function setActiveSection(menuItemID) {
-    removeAllActiveSections();
-    const navItem = document.getElementById(menuItemID);
-    navItem.classList.add("active-section");
-}
+// function setActiveSection(menuItemID) {
+//     removeAllActiveSections();
+//     const navItem = document.getElementById(menuItemID);
+//     navItem.classList.add("active-section");
+// }
 
-// scroll observer --------
+// // scroll observer --------
 
-function sectionsObserverCallback(entries) {
-    entries.forEach((entry) => {
-        const {
-            target: { id },
-            isIntersecting,
-        } = entry;
-        if (isIntersecting) {
-            const navItems = document.querySelectorAll(`a[href='#${id}']`);
-            const navItemId = navItems[0]?.id;
-            setActiveSection(navItemId);
-        }
-    });
-}
+// function sectionsObserverCallback(entries) {
+//     entries.forEach((entry) => {
+//         const {
+//             target: { id },
+//             isIntersecting,
+//         } = entry;
+//         if (isIntersecting) {
+//             const navItems = document.querySelectorAll(`a[href='#${id}']`);
+//             const navItemId = navItems[0]?.id;
+//             setActiveSection(navItemId);
+//         }
+//     });
+// }
 
-const observerSections = new IntersectionObserver(sectionsObserverCallback, {
-    root: null,
-    threshold: 0.5,
-});
+// const observerSections = new IntersectionObserver(sectionsObserverCallback, {
+//     root: null,
+//     threshold: 0.5,
+// });
 
-const sections = [...document.querySelectorAll(".js-content-section")];
-sections.forEach((section) => observerSections.observe(section));
+// const sections = [...document.querySelectorAll(".js-content-section")];
+// sections.forEach((section) => observerSections.observe(section));
 
-window.addEventListener("scroll", function (e) {
-    lastKnownScrollPosition = window.scrollY;
-    if (lastKnownScrollPosition <= 0) {
-        document
-            .getElementById("header")
-            .classList.remove("header-menu-animation");
+// window.addEventListener("scroll", function (e) {
+//     lastKnownScrollPosition = window.scrollY;
+//     if (lastKnownScrollPosition <= 0) {
+//         document
+//             .getElementById("header")
+//             .classList.remove("header-menu-animation");
+//     } else {
+//         document
+//             .getElementById("header")
+//             .classList.add("header-menu-animation");
+//     }
+// });
+
+function showMenu() {
+    if (document.getElementById("header-nav").style.display === "none") {
+        document.getElementById("header-nav").style.display = "flex";
+        let headerNav = document.getElementById("header-nav");
+        document.getElementById("menu-icon").style.width = window
+            .getComputedStyle(headerNav)
+            .getPropertyValue("width");
+        document.getElementById("menu-icon").style.borderBottomLeftRadius =
+            "0px";
     } else {
-        document
-            .getElementById("header")
-            .classList.add("header-menu-animation");
+        hideMenu();
     }
-});
+}
+
+function hideMenu() {
+    document.getElementById("header-nav").style.display = "none";
+    document.getElementById("menu-icon").style.width = "60px";
+    document.getElementById("menu-icon").style.borderBottomLeftRadius = "10px";
+}
